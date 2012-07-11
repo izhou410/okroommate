656b0cee8e21e9164a4f0b90cd292ca2af2d63e8
izhou410 <izhou@mit.edu>
2012-07-10 21:33:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Okroommate::Application.config.secret_token = '3822bdc9fda908de701ab7b0f400bb6484ed4dacfac7db122d5421d6197d1f4098220b389959d87f8dd2f675a7eb99936bc7218f6c1e4c788d8911fb535ac8ff'
